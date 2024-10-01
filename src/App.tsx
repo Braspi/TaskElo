2ad352f2b7c2093cc935bd8e41108d988bc1b0f1
jeremiasz <shutmer@gmail.com>
2024-10-01 07:15:23 +0200

    feat(app): add new layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ModalTask from "./componets/modalTask";
+
 
 function App() {
+    const [data, setData] = useState<string[]>([])
+    useEffect(() => {
+        setData(["Upcoming", "In Progress", "Done"])
+    }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container text-center m-sm-4">
+        <div className="row align-items-start d-flex">
+            {data.map(it => (
+            <div className="col">
+              <div className="card">
+                      <div className="card-header">
+                          {it}
+                      </div>
+                  <div className="card-body border m-sm-4 rounded-2 ">
+                    <div className="bg-warning p-4"></div>
+                    <p>Plan marketing campaign</p>
+                   <ModalTask/>
+                  </div>
+              </div>
+            </div>
+            ))}
+        </div>
+      </div>
   );
 }
 
