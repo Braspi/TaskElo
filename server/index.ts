139b27a2a2049da07c666504df92faa5b6e1661b
ponurakk <p0unrakg@int.pl>
2024-10-06 22:44:01 +0200

    refactor: Remove redundant code

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,11 @@
 import express from "express";
 import fs from "node:fs";
-import { Module, Task } from "../types";
 
 const app = express();
 const port = 3000;
 
 try {
-  ["./data/users.json", "./data/tasks.json"].forEach((file, i) => {
+  ["./data/users.json", "./data/tasks.json"].forEach(file => {
     if (!fs.existsSync(file)) {
       fs.writeFileSync(file, "[]");
       console.log(`Created ${file}`);
@@ -17,11 +16,11 @@ try {
   process.exit(1);
 }
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("Hello :D");
 });
 
-app.get("/users", (req, res) => {
+app.get("/users", (_req, res) => {
   try {
     const data = fs.readFileSync("./data/users.json", 'utf8');
     res.json(JSON.parse(data));
@@ -30,7 +29,7 @@ app.get("/users", (req, res) => {
   }
 });
 
-app.get("/tasks", (req, res) => {
+app.get("/tasks", (_req, res) => {
   try {
     const data = fs.readFileSync("./data/tasks.json", 'utf8');
     res.json(JSON.parse(data));
@@ -42,79 +41,3 @@ app.get("/tasks", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at :${port}`);
 });
-
-let task: Task[] = [
-  {
-    title: "Plan marketing campaingn",
-    taskGroup: "Upcoming",
-    tags: [
-      { name: "Marketing", color: "#FF9E1C" },
-    ],
-    duedate: new Date("2024-09-20"),
-    assignedUsers: [1, 2],
-    modules: [
-      new Module("TextModule", { text: "lorem ipsum" }),
-      new Module("TasksModule", {
-        tasks: [
-          { title: "Task 1", description: "lorem ipsum", isDone: false },
-          { title: "Task 2", description: "lorem ipsum", isDone: false },
-          { title: "Task 3", description: "lorem ipsum", isDone: false }
-        ]
-      })
-    ]
-  },
-  {
-    title: "Approve legal agreement",
-    taskGroup: "Upcoming",
-    tags: [
-      { name: "Legal", color: "#53E897" },
-    ],
-    duedate: null,
-    assignedUsers: [1, 2],
-    modules: [
-      new Module("TextModule", { text: "lorem ipsum" }),
-    ]
-  },
-  {
-    title: "Schedule recurring client meeting",
-    taskGroup: "Upcoming",
-    tags: [
-      { name: "Marketing", color: "#FF9E1C" },
-    ],
-    duedate: null,
-    assignedUsers: [1, 2],
-    modules: [
-      new Module("TextModule", { text: "lorem ipsum" }),
-      new Module("AttachmentModule", { name: "File", filePath: "" }),
-    ]
-  },
-
-  {
-    title: "Collect design assets",
-    taskGroup: "In Progress",
-    tags: [
-      { name: "External", color: "#F0D804" },
-      { name: "Design", color: "#04C1DF" },
-    ],
-    duedate: null,
-    assignedUsers: [1, 2],
-    modules: [
-      new Module("TextModule", { text: "lorem ipsum" }),
-    ]
-  },
-
-  {
-    title: "Collect design assets",
-    taskGroup: "Done",
-    tags: [
-      { name: "External", color: "#F0D804" },
-      { name: "Design", color: "#04C1DF" },
-    ],
-    duedate: null,
-    assignedUsers: [1, 2],
-    modules: [
-      new Module("TextModule", { text: "lorem ipsum" }),
-    ]
-  },
-]
-console.log(JSON.stringify(task));
