87df116c03fc19e62e70175415b7b1935a8199a6
Braspi <shutmer@gmail.com>
2024-10-06 15:19:07 +0200

    feat(app): add content in tasks.json

--- src/componets/tags/design.tag.tsx
+++ src/componets/tags/design.tag.tsx
@@ -2,7 +2,6 @@ export default function designTag() {
   return (
     <>
       <div className="bg-blue-500 p-1 px-2 rounded-md">Design</div>
-      <div className="bg-green-500 p-1 px-2 rounded-md">Sales</div>
     </>
   );
 };
