eb688438783122962b01f3984626dfe1f1fb6111
jeremiasz <shutmer@gmail.com>
2024-10-07 12:36:02 +0200

    fix(app): fixed color

--- src/componets/addons/time.addon.tsx
+++ src/componets/addons/time.addon.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 export default function TimeAddon(props: Props) {
   const formattedDate = new Date(props.date).toLocaleString('en-US', { month: "short", day: "numeric" });
-  const color = new Date(props.date) < new Date() ? "bg-green-900" : "";
+  const color = new Date(props.date) < new Date() ? "bg-green-500" : "";
   return (
-    <div className={`flex gap-1 justify-center items-center rounded p-1 ${color}`}>
+    <div className={`flex gap-1 justify-center items-center rounded p-1 px-2 ${color}`}>
       <ClockIcon className="size-6" />
       <span>{formattedDate}</span>
     </div>
